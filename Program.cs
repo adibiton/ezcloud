22dde236631e13c3b240a75092f90c914a6a6e5e
Adi Biton <adbiton@microsoft.com>
2019-07-22 10:35:32 +0300

    feat(): first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ezcloud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
